c08cf659f3087c76a9c7c1f659c4423324309fe4
Ольга Мелихова <melihova.o@yandex.ru>
2023-05-04 16:43:19 +0300

    backend-users-model-feedback: create model

--- backend/spanish_with_anna/users/admin.py
+++ backend/spanish_with_anna/users/admin.py
@@ -1,17 +1,3 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
 
-from users.models import CustomUser, Feedback
-
-User = get_user_model()
-
-
-@admin.register(CustomUser)
-class AdminCustomUser(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Feedback)
-class AdminFeedback(admin.ModelAdmin):
-    list_display = ('name', 'email', 'phone', 'message', 'user', 'time_create')
-    list_filter = ('email', 'time_create', 'user')
+# Register your models here.
