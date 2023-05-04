a7d4be780301afd039c864438cabb14dd2fa121d
Ольга Мелихова <melihova.o@yandex.ru>
2023-05-04 17:11:55 +0300

    backend-users-model-feedback: create model

--- backend/spanish_with_anna/users/admin.py
+++ backend/spanish_with_anna/users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from users.models import Feedback
+
+User = get_user_model()
+
+
+@admin.register(Feedback)
+class AdminFeedback(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'time_create', 'user', 'message')
+    list_filter = ('email', 'time_create', 'user')
+    search_fields = ('email', 'message')
