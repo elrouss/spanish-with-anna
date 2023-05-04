60935bf573c9a2ddbde1a30642840b4405b98b94
Ольга Мелихова <melihova.o@yandex.ru>
2023-05-04 16:16:59 +0300

    backend/users/model-feedback: create model

--- backend/spanish_with_anna/users/admin.py
+++ backend/spanish_with_anna/users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from users.models import CustomUser, Feedback
+
+User = get_user_model()
+
+
+@admin.register(CustomUser)
+class AdminCustomUser(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Feedback)
+class AdminFeedback(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone', 'message', 'user', 'time_create')
+    list_filter = ('email', 'time_create', 'user')
--- backend/spanish_with_anna/users/models.py
+++ backend/spanish_with_anna/users/models.py
@@ -1,5 +1,9 @@
+import phonenumbers
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+from django.core.validators import EmailValidator
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 
 class CustomUser(AbstractUser):
@@ -9,4 +13,60 @@ class CustomUser(AbstractUser):
 
 class Feedback(models.Model):
     """Модель обратной связи"""
-    pass
+    name = models.CharField(
+        max_length=64,
+        verbose_name='Имя',
+    )
+    email = models.EmailField(
+        max_length=256,
+        verbose_name='Email',
+        validators=[EmailValidator(
+            message='Неправильный формат адреса электронной почты')],
+    )
+    phone = PhoneNumberField(
+        blank=True,
+        null=True,
+        verbose_name='Телефон',
+    )
+    message = models.TextField(
+        verbose_name='Сообщение',
+    )
+    time_create = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата отправки'
+        )
+    user = models.ForeignKey(
+        CustomUser,
+        verbose_name='Пользователь',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+
+    def clean(self):
+        super().clean()
+        if self.phone:
+            try:
+                parsed_number = phonenumbers.parse(self.phone.as_e164)
+                if not phonenumbers.is_valid_number(parsed_number):
+                    raise ValidationError(
+                        'Неверный формат телефонного номера.'
+                        )
+            except phonenumbers.phonenumberutil.NumberParseException:
+                raise ValidationError('Неверный формат телефонного номера.')
+
+    def save(self, *args, **kwargs):
+        users_by_email = CustomUser.objects.filter(email=self.email)
+        if users_by_email.count() > 0:
+            self.user = users_by_email[0]
+            super().save(*args, **kwargs)
+        else:
+            super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Обратная связь'
+        verbose_name_plural = 'Обратная связь'
+        ordering = ['-time_create']
+
+    def __str__(self):
+        return f'Вам письмо от {self.email}'
