36f4074db6f194bed82bc52c9503e7f724513a08
Svetlana Logvinova <sunny7-7-7@yandex.ru>
2023-05-13 12:53:53 +0300

    backend-users-model-custom-user feat:implement model customuser

--- backend/spanish_with_anna/users/admin.py
+++ backend/spanish_with_anna/users/admin.py
@@ -1,9 +1,16 @@
+from django import forms
 from django.contrib import admin
-from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Group
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
 
-from users.models import Feedback
+from users.models import CustomUser, Feedback
 
-User = get_user_model()
+
+admin.site.site_header = 'Администрирование сайта Spanish With Anna'
+admin.site.index_title = 'Добро пожаловать!'
+admin.site.site_title = 'Spanish With Anna Административный портал'
 
 
 @admin.register(Feedback)
@@ -11,3 +18,124 @@ class AdminFeedback(admin.ModelAdmin):
     list_display = ('name', 'email', 'phone', 'time_create', 'user', 'message')
     list_filter = ('email', 'time_create', 'user')
     search_fields = ('email', 'message')
+
+
+class CustomUserCreationForm(forms.ModelForm):
+    """
+    Форма для создания пользователя.
+    """
+
+    password1 = forms.CharField(
+        label='Пароль',
+        widget=forms.PasswordInput,
+    )
+    password2 = forms.CharField(
+        label='Подтверждение пароля',
+        widget=forms.PasswordInput,
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'name', 'role', 'phone',)
+
+    def clean_password2(self):
+        """
+        Функция проверяет, что введенные пароли совпадают.
+        """
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Указанные пароли нес совпадают.")
+
+        return password2
+
+    def save(self, commit=True):
+        """
+        Функция создает пользователя и сохраняет пароль в хешированном виде.
+        """
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+
+        return user
+
+
+class CustomUserChangeForm(forms.ModelForm):
+    """
+    Форма для обновления пользователей. Включает все поля пользователя.
+    """
+
+    password = ReadOnlyPasswordHashField(label=(" Пароль"), help_text=(
+        "Просмотреть пароль пользователя невозможно. Для изменения "
+        "пароля используйте <a href=\"../password/\">эту форму</a>."))
+
+    class Meta:
+        model = CustomUser
+        fields = (
+            'name', 'email', 'password', 'phone', 'role', 'courses',
+            'is_active', 'is_staff', 'is_superuser',
+        )
+
+
+class CustomUserAdmin(BaseUserAdmin):
+    """
+    Класс для настройки отображения модели пользователя.
+    """
+
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+
+    # Эти поля отображаются при изменении пользователя администратором.
+    list_display = (
+        'name', 'email', 'phone', 'role', 'password',
+    )
+    list_filter = ('email',)
+    fieldsets = (
+        (None, {
+            'fields': ('email', 'name', 'phone', )
+        }),
+        ('Personal info', {
+            'fields': ('role', 'password',)
+        }),
+        ('Permissions', {
+            'fields': (
+                'is_active', 'is_staff', 'is_superuser', 'user_permissions',
+            )
+        }),
+        ('Important dates', {
+            'fields': ('last_login', 'date_joined',)
+        }),
+    )
+
+    # Поля, используемые админиcтратором для создания пользователя.
+    add_fieldsets = (
+        (None, {
+            'fields': (
+                'email', 'name', 'phone', 'role', 'password1', 'password2',
+            ),
+        }),
+        (('Permissions'), {
+            'fields': (
+                'is_active', 'is_staff', 'is_superuser', 'user_permissions',
+            )
+        }),
+        (('Important dates'), {
+            'fields': ('last_login', 'date_joined',)
+        }),
+    )
+
+    readonly_fields = (
+        'date_joined',
+        'last_login',
+    )
+
+    list_filter = ('email', 'role')
+    search_fields = ['email']
+    ordering = ['email']
+    filter_horizontal = []
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
+admin.site.unregister(Group)
--- backend/spanish_with_anna/users/models.py
+++ backend/spanish_with_anna/users/models.py
@@ -1,35 +1,122 @@
-import phonenumbers
 from django.conf import settings
-from django.contrib.auth.models import AbstractUser
-from django.core.exceptions import ValidationError
-from django.core.validators import EmailValidator
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 from phonenumber_field.modelfields import PhoneNumberField
 
+from courses.models import Course
+from users.managers import CustomUserManager
 
-class CustomUser(AbstractUser):
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     """
     Модель для описания пользователя.
     """
-    pass
+
+    USER = 'user'
+    STUDENT = 'student'
+    ADMIN = 'admin'
+
+    ROLE_CHOICES = (
+        (USER, 'Пользователь'),
+        (STUDENT, 'Студент'),
+        (ADMIN, 'Администратор')
+    )
+
+    name = models.CharField(
+        'Имя пользователя',
+        max_length=64,
+    )
+    email = models.EmailField(
+        'Адрес e-mail',
+        max_length=254,
+        unique=True,
+        error_messages={
+            'unique': 'Пользователь с указанным e-mail уже существует.',
+        }
+    )
+    phone = PhoneNumberField(
+        'Номер телефона',
+        blank=True,
+        null=True,
+    )
+    role = models.CharField(
+        'Роль пользователя',
+        choices=ROLE_CHOICES,
+        max_length=15,
+        default=USER,
+    )
+    courses = models.ManyToManyField(
+        Course,
+        verbose_name='Курсы пользователя',
+        related_name='courses',
+    )
+    is_active = models.BooleanField(
+        'Статус пользователя (активный/неактивный)',
+        default=True,
+    )
+    is_staff = models.BooleanField(
+        'Доступ к админке Django (да/нет)',
+        default=False,
+    )
+    is_superuser = models.BooleanField(
+        'Суперпользователь (да/нет)',
+        default=False,
+    )
+    date_joined = models.DateTimeField(
+        'Дата регистрации',
+        auto_now_add=True,
+    )
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = [
+        'name',
+    ]
+
+    EMAIL_FIELD = 'email'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('email',)
+
+    def __str__(self):
+        return f'{self.email}'
+
+    @property
+    def is_admin(self):
+        return self.role == CustomUser.ADMIN
+
+    @property
+    def is_student(self):
+        return self.role == CustomUser.STUDENT
+
+    @property
+    def is_user(self):
+        return self.role == CustomUser.USER
+
+    def get_full_name(self):
+        """
+        Функция возвращает полное имя пользователя.
+        """
+        return self.name
 
 
 class Feedback(models.Model):
     """
     Модель для описания обратной связи.
     """
+
     name = models.CharField(
         'Имя',
         max_length=64,
     )
     email = models.EmailField(
         'Email',
-        max_length=256,
-        validators=[
-            EmailValidator(
-                message='Неправильный формат адреса электронной почты'
-                )
-            ],
+        max_length=254,
     )
     phone = PhoneNumberField(
         'Телефон',
@@ -38,31 +125,28 @@ class Feedback(models.Model):
     )
     message = models.TextField(
         'Сообщение',
+        max_length=1024,
     )
     time_create = models.DateTimeField(
         'Дата отправки',
         auto_now_add=True,
         )
     user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
         verbose_name='Пользователь',
-        to=settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         blank=True,
         null=True,
         related_name='feedbacks',
     )
 
-    def clean(self):
-        super().clean()
-        if self.phone:
-            try:
-                parsed_number = phonenumbers.parse(self.phone.as_e164)
-                if not phonenumbers.is_valid_number(parsed_number):
-                    raise ValidationError(
-                        'Неверный формат телефонного номера.'
-                        )
-            except phonenumbers.phonenumberutil.NumberParseException:
-                raise ValidationError('Неверный формат телефонного номера.')
+    class Meta:
+        verbose_name = 'Обратная связь'
+        verbose_name_plural = 'Обратная связь'
+        ordering = ['-time_create',]
+
+    def str(self):
+        return f'Вам письмо от {self.email}'
 
     def save(self, *args, **kwargs):
         users_by_email = CustomUser.objects.filter(email=self.email)
@@ -71,11 +155,3 @@ class Feedback(models.Model):
             super().save(*args, **kwargs)
         else:
             super().save(*args, **kwargs)
-
-    class Meta:
-        verbose_name = 'Обратная связь'
-        verbose_name_plural = 'Обратная связь'
-        ordering = ['-time_create']
-
-    def str(self):
-        return f'Вам письмо от {self.email}'
