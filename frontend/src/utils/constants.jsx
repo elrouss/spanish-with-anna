8a160e2c3c3d785514eff9ca086b64da11a55e6c
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-06-05 19:16:11 +0300

    feat: add facts list

---
+++ frontend/src/utils/constants.jsx
@@ -0,0 +1,16 @@
+export const FACTS = [
+  {
+    counter: '10+',
+    description: 'лет преподавания',
+  },
+  {
+    counter: '130+',
+    description: 'счастливых учеников',
+  },
+  {
+    counter: '2000+',
+    description: 'испаноязычных туристов',
+  },
+];
+
+export const TEMP = 'temp'; // TODO: Удалить, когда появтся больше чем одна константа
