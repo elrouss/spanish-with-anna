b01d13f9abec68363a95c1f465d6137139a18872
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-06-05 19:26:30 +0300

    feat: add fact id

--- frontend/src/utils/constants.jsx
+++ frontend/src/utils/constants.jsx
@@ -1,13 +1,16 @@
 export const FACTS = [
   {
+    _id: '0001',
     counter: '10+',
     description: 'лет преподавания',
   },
   {
+    _id: '0002',
     counter: '130+',
     description: 'счастливых учеников',
   },
   {
+    _id: '0003',
     counter: '2000+',
     description: 'испаноязычных туристов',
   },
