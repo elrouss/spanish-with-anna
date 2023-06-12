5c8a2f12e8b6637966c6acd466f83a72fbf78376
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-06-13 00:37:24 +0300

    feat: add info cards data

---
+++ frontend/src/utils/data/infoCards.jsx
@@ -0,0 +1,28 @@
+const infoCards = [
+  {
+    _id: '0001',
+    title: 'Занятия в группах',
+    description: `Интерактивные занятия
+в мини-группах до 4-х человек`,
+    image: '@/assets/images/online-courses-info-1.jpg',
+    link: '#',
+  },
+  {
+    _id: '0002',
+    title: 'Самостоятельное обучение',
+    description: `Совершенствуйте свой испанский
+в удобном для вас темпе`,
+    image: '@/assets/images/online-courses-info-2.jpg',
+    link: '#',
+  },
+  {
+    _id: '0003',
+    title: 'Персональное обучение',
+    description: `Индивидуальные онлайн-уроки,
+менторство, консультации`,
+    image: '@/assets/images/online-courses-info-3.jpg',
+    link: '#',
+  },
+];
+
+export default infoCards;
