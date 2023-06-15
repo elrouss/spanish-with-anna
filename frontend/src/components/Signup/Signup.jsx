66b3ee3b53b45d59163885d513a7f2d671908d0b
yryryk <tovchennikov@e1.ru>
2023-06-15 15:24:42 +0500

    feat: add bonus gift

--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import imageRegistration from '../../assets/images/intro-image-course-registration.png';
 import styles from './signup.module.scss';
 import Button from '../Button/Button';
+import IconArrowRegistration from '../../assets/icons/arrow-registration';
+import IconGift from '../../assets/icons/gift';
 
 export default function Signup() {
   return (
@@ -17,13 +19,15 @@ export default function Signup() {
         <div className={styles.container}>
           <h2 className={styles.title}>как записаться на курс</h2>
           <div className={styles.subContainer}>
-            <p className={styles.text}>Запись на любой курс доступна только</p>
+            <p className={styles.text}>
+              Запись на&nbsp;любой курс доступна только
+            </p>
             <span className={styles.text}>
               <span className={styles.selectedText}>зарегистрированным</span>
               пользователям.
             </span>
             <p className={styles.text}>
-              Вы получите подборку лучших инструментов
+              Вы&nbsp;получите подборку лучших инструментов
               для&nbsp;изучения&nbsp;языка:
             </p>
             <ul className={styles.list}>
@@ -40,6 +44,17 @@ export default function Signup() {
           <Button classSelector={`${styles.button} button`} href="/">
             Зарегистрироваться
           </Button>
+        </div>
+      </div>
+      <div className={styles.bonus}>
+        <p className={styles.iconText}>
+          После регистрации вас ждет приятный бонус!
+        </p>
+        <div className={styles.iconGift}>
+          <IconGift />
+        </div>
+        <div className={styles.arrow}>
+          <IconArrowRegistration />
         </div>
       </div>
     </section>
