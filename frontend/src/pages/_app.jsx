fa071b7311ec10a46cb011d1fe49f4fc13b35207
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-06-18 23:22:58 +0300

    fix: remove error with export in _app

--- frontend/src/pages/_app.jsx
+++ frontend/src/pages/_app.jsx
@@ -5,7 +5,7 @@ import '@/styles/globals.scss';
 
 import { manrope, raleway } from '@/assets/fonts/fonts';
 
-export default function App({ Component, pageProps }) {
+function App({ Component, pageProps }) {
   return (
     <div className={`${manrope.variable} ${raleway.variable}`}>
       <Component {...pageProps} />
