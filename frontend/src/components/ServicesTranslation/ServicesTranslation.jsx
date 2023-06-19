f6aa0de060e05ce530134cc8854dd02b6d3c5722
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-06-19 15:43:08 +0300

    refactor: change grid for section

--- frontend/src/components/ServicesTranslation/ServicesTranslation.jsx
+++ frontend/src/components/ServicesTranslation/ServicesTranslation.jsx
@@ -7,34 +7,36 @@ import translationImage from '../../../public/assets/images/translation-book.jpg
 function ServicesTranslation() {
   return (
     <section className={styles.servicesTranslation}>
-      <div className={styles.container}>
-        <div className={styles.info}>
+      <div className={styles.containerOuter}>
+        <div className={styles.containerInner}>
+          <div className={styles.info}>
+            <Image
+              className={styles.translationIcon}
+              width={62}
+              height={53}
+              src={translationIcon}
+              alt="Перевести"
+            />
+            <h2 className={styles.title}>Нужно перевести?</h2>
+            <p className={styles.description}>
+              Вы&nbsp;всегда можете обратиться ко&nbsp;мне не&nbsp;только
+              с&nbsp;целью освоить язык, но&nbsp;и&nbsp;перевести текст любой
+              сложности (рус-исп или исп-рус). Я&nbsp;гарантирую вам
+              профессионализм, высокую точность и&nbsp;скорость перевода.
+            </p>
+            <Button classSelector={styles.button} href="#">
+              Заказать перевод
+            </Button>
+            <div className={styles.decorativeTile} />
+          </div>
           <Image
-            className={styles.translationIcon}
-            width={62}
-            height={53}
-            src={translationIcon}
-            alt="Перевести"
+            className={styles.translationImage}
+            width={676}
+            height={584}
+            src={translationImage}
+            alt="Запись в тетрадь"
           />
-          <h2 className={styles.title}>Нужно перевести?</h2>
-          <p className={styles.description}>
-            Вы&nbsp;всегда можете обратиться ко&nbsp;мне не&nbsp;только
-            с&nbsp;целью освоить язык, но&nbsp;и&nbsp;перевести текст любой
-            сложности (рус-исп или исп-рус). Я&nbsp;гарантирую вам
-            профессионализм, высокую точность и&nbsp;скорость перевода.
-          </p>
-          <Button classSelector={styles.button} href="#">
-            Заказать перевод
-          </Button>
-          <div className={styles.decorativeTile} />
         </div>
-        <Image
-          className={styles.translationImage}
-          width={676}
-          height={584}
-          src={translationImage}
-          alt="Запись в тетрадь"
-        />
       </div>
     </section>
   );
