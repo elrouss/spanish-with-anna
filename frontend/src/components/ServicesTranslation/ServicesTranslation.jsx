5a1816c47ae4247647953245ac2844e8241a8b66
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-06-19 13:30:26 +0300

    feat: add images to markup

--- frontend/src/components/ServicesTranslation/ServicesTranslation.jsx
+++ frontend/src/components/ServicesTranslation/ServicesTranslation.jsx
@@ -1,12 +1,20 @@
 import Image from 'next/image';
 import styles from './ServicesTranslation.module.scss';
 import Button from '../Button/Button';
+import translationIcon from '../../../public/assets/icons/translation.svg';
+import translationImage from '../../../public/assets/images/translation-book.jpg';
 
 function ServicesTranslation() {
   return (
     <section className={styles.servicesTranslation}>
-      {/* <div className={styles.info}>
-        <Image />
+      <div className={styles.info}>
+        <Image
+          className={styles.translationIcon}
+          width={62}
+          height={53}
+          src={translationIcon}
+          alt="Перевести"
+        />
         <h2 className={styles.title}>Нужно перевести?</h2>
         <p className={styles.desctiption}>
           Вы&nbsp;всегда можете обратиться ко&nbsp;мне не&nbsp;только
@@ -14,9 +22,17 @@ function ServicesTranslation() {
           сложности (рус-исп или исп-рус). Я&nbsp;гарантирую вам
           профессионализм, высокую точность и&nbsp;скорость перевода.
         </p>
-        <Button />
+        <Button classSelector={styles.button} href="#">
+          Заказать перевод
+        </Button>
       </div>
-      <Image /> */}
+      <Image
+        className={styles.translationImage}
+        width={676}
+        height={584}
+        src={translationImage}
+        alt="Запись в тетрадь"
+      />
     </section>
   );
 }
