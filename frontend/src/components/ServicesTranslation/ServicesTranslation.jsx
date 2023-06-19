ac2293d80c12c396e3259e4138470b26b3a8a623
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-06-19 12:33:24 +0300

    feat: add markup

--- frontend/src/components/ServicesTranslation/ServicesTranslation.jsx
+++ frontend/src/components/ServicesTranslation/ServicesTranslation.jsx
@@ -1,7 +1,24 @@
+import Image from 'next/image';
 import styles from './ServicesTranslation.module.scss';
+import Button from '../Button/Button';
 
 function ServicesTranslation() {
-  return <>temp</>;
+  return (
+    <section className={styles.servicesTranslation}>
+      {/* <div className={styles.info}>
+        <Image />
+        <h2 className={styles.title}>Нужно перевести?</h2>
+        <p className={styles.desctiption}>
+          Вы&nbsp;всегда можете обратиться ко&nbsp;мне не&nbsp;только
+          с&nbsp;целью освоить язык, но&nbsp;и&nbsp;перевести текст любой
+          сложности (рус-исп или исп-рус). Я&nbsp;гарантирую вам
+          профессионализм, высокую точность и&nbsp;скорость перевода.
+        </p>
+        <Button />
+      </div>
+      <Image /> */}
+    </section>
+  );
 }
 
 export default ServicesTranslation;
