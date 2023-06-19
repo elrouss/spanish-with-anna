cb7cb80162f2daa733a6f9e62cb2b1eb9e7c52e9
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-06-19 17:52:30 +0300

    refactor: change styles (new layout)

--- frontend/src/components/ServicesTranslation/ServicesTranslation.jsx
+++ frontend/src/components/ServicesTranslation/ServicesTranslation.jsx
@@ -2,41 +2,32 @@ import Image from 'next/image';
 import Button from '../Button/Button';
 import styles from './ServicesTranslation.module.scss';
 import translationIcon from '../../../public/assets/icons/translation.svg';
-import translationImage from '../../../public/assets/images/translation-book.jpg';
 
 function ServicesTranslation() {
   return (
     <section className={styles.servicesTranslation}>
-      <div className={styles.containerOuter}>
-        <div className={styles.containerInner}>
-          <div className={styles.info}>
-            <Image
-              className={styles.translationIcon}
-              width={62}
-              height={53}
-              src={translationIcon}
-              alt="Перевести"
-            />
-            <h2 className={styles.title}>Нужно перевести?</h2>
-            <p className={styles.description}>
-              Вы&nbsp;всегда можете обратиться ко&nbsp;мне не&nbsp;только
-              с&nbsp;целью освоить язык, но&nbsp;и&nbsp;перевести текст любой
-              сложности (рус-исп или исп-рус). Я&nbsp;гарантирую вам
-              профессионализм, высокую точность и&nbsp;скорость перевода.
-            </p>
-            <Button classSelector={styles.button} href="#">
-              Заказать перевод
-            </Button>
-            <div className={styles.decorativeTile} />
-          </div>
+      <div className={styles.container}>
+        <div className={styles.info}>
           <Image
-            className={styles.translationImage}
-            width={676}
-            height={584}
-            src={translationImage}
-            alt="Запись в тетрадь"
+            className={styles.translationIcon}
+            width={62}
+            height={53}
+            src={translationIcon}
+            alt="Перевести"
           />
+          <h2 className={styles.title}>Нужно перевести?</h2>
+          <p className={styles.description}>
+            Вы&nbsp;всегда можете обратиться ко&nbsp;мне не&nbsp;только
+            с&nbsp;целью освоить язык, но&nbsp;и&nbsp;перевести текст любой
+            сложности (рус-исп или исп-рус). Я&nbsp;гарантирую вам
+            профессионализм, высокую точность и&nbsp;скорость перевода.
+          </p>
+          <Button classSelector={styles.button} href="#">
+            Заказать перевод
+          </Button>
+          <div className={styles.decorativeTile} />
         </div>
+        <div className={styles.translationImage} />
       </div>
     </section>
   );
