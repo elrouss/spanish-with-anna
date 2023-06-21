9d38c66d172703837fca8ebabc63943a2c096869
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-06-21 16:46:54 +0300

    feat: add faq block

--- frontend/src/components/FAQ/Question.jsx
+++ frontend/src/components/FAQ/Question.jsx
@@ -1,7 +1,47 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import Image from 'next/image';
+import iconArrowDown from '../../../public/assets/icons/arrow-down.svg';
 import styles from './FAQ.module.scss';
 
-function Question() {
-  return <div className={styles.question} />;
+function Question({ title, answer }) {
+  const [isAnswerVisible, setIsAnswerVisible] = useState(false);
+
+  function toggleAnswerVisibility() {
+    setIsAnswerVisible(!isAnswerVisible);
+  }
+  return (
+    <>
+      <button
+        className={`${styles.accordionItemHead} ${
+          isAnswerVisible && styles.accordionItemHead_active
+        }`}
+        type="button"
+        onClick={toggleAnswerVisibility}
+      >
+        <h3 className={styles.accordionItemTitle}>{title}</h3>
+        <Image
+          className={`${styles.arrowDown} ${isAnswerVisible && styles.arrowUp}`}
+          src={iconArrowDown}
+          width={23}
+          height={13}
+          alt="Показать ответ"
+        />
+      </button>
+      <div
+        className={`${styles.accordionItemContent} ${
+          isAnswerVisible && styles.accordionItemContent_active
+        }`}
+      >
+        {answer}
+      </div>
+    </>
+  );
 }
+
+Question.propTypes = {
+  title: PropTypes.string.isRequired,
+  answer: PropTypes.string.isRequired,
+};
 
 export default Question;
---
+++ frontend/src/utils/data/FAQ_DATA.jsx
@@ -0,0 +1,52 @@
+const FAQ_DATA = [
+  {
+    _id: 1,
+    title: 'Какие курсы мне подойдут?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias, earum sit hic repellat omnis aspernatur delectus perspiciatis voluptatibus laudantium exercitationem suscipit adipisci ipsum maxime, amet, autem dolores error itaque iste! Sit impedit nisi alias delectus ullam repudiandae, dignissimos pariatur porro incidunt fuga, provident quos laborum dolore, omnis sint! Molestias fugit quos quas dolorum ratione obcaecati, distinctio repudiandae voluptatum cupiditate autem. Iure quod cum ad vel deserunt at magni, ullam, dolorum sunt atque sit, doloremque deleniti sed expedita suscipit tenetur laboriosam rerum. Eaque possimus culpa dicta asperiores eligendi corrupti, veniam repellendus.',
+  },
+  {
+    _id: 2,
+    title: 'На какой уровень рассчитаны курсы?',
+    answer:
+      'Курсы для самостоятельного обучения имеют отметку, для какого уровня они лучше всего подойдут. Для записи на занятия в группах просто укажите ваш уровень в заявке. Я подберу для вас наиболее подходящую языковую группу. Если вы сомневаетесь в своем уровне, просто напишите мне, заполнив форму “задать вопрос”.',
+  },
+  {
+    _id: 3,
+    title: 'Какова продолжительность курса?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias, earum sit hic repellat omnis aspernatur delectus perspiciatis voluptatibus laudantium exercitationem suscipit adipisci ipsum maxime, amet, autem dolores error itaque iste! Sit impedit nisi alias delectus ullam repudiandae, dignissimos pariatur porro incidunt fuga, provident quos laborum dolore, omnis sint! Molestias fugit quos quas dolorum ratione obcaecati, distinctio repudiandae voluptatum cupiditate autem. Iure quod cum ad vel deserunt at magni, ullam, dolorum sunt atque sit, doloremque deleniti sed expedita suscipit tenetur laboriosam rerum. Eaque possimus culpa dicta asperiores eligendi corrupti, veniam repellendus.',
+  },
+  {
+    _id: 4,
+    title: 'Что, если я не нашел лекций по нужной мне теме?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias, earum sit hic repellat omnis aspernatur delectus perspiciatis voluptatibus laudantium exercitationem suscipit adipisci ipsum maxime, amet, autem dolores error itaque iste! Sit impedit nisi alias delectus ullam repudiandae, dignissimos pariatur porro incidunt fuga, provident quos laborum dolore, omnis sint! Molestias fugit quos quas dolorum ratione obcaecati, distinctio repudiandae voluptatum cupiditate autem. Iure quod cum ad vel deserunt at magni, ullam, dolorum sunt atque sit, doloremque deleniti sed expedita suscipit tenetur laboriosam rerum. Eaque possimus culpa dicta asperiores eligendi corrupti, veniam repellendus.',
+  },
+  {
+    _id: 5,
+    title: 'Почему я могу доверять Анне?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias, earum sit hic repellat omnis aspernatur delectus perspiciatis voluptatibus laudantium exercitationem suscipit adipisci ipsum maxime, amet, autem dolores error itaque iste! Sit impedit nisi alias delectus ullam repudiandae, dignissimos pariatur porro incidunt fuga, provident quos laborum dolore, omnis sint! Molestias fugit quos quas dolorum ratione obcaecati, distinctio repudiandae voluptatum cupiditate autem. Iure quod cum ad vel deserunt at magni, ullam, dolorum sunt atque sit, doloremque deleniti sed expedita suscipit tenetur laboriosam rerum. Eaque possimus culpa dicta asperiores eligendi corrupti, veniam repellendus.',
+  },
+  {
+    _id: 6,
+    title: 'Чем эти курсы отличаются от других?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias, earum sit hic repellat omnis aspernatur delectus perspiciatis voluptatibus laudantium exercitationem suscipit adipisci ipsum maxime, amet, autem dolores error itaque iste! Sit impedit nisi alias delectus ullam repudiandae, dignissimos pariatur porro incidunt fuga, provident quos laborum dolore, omnis sint! Molestias fugit quos quas dolorum ratione obcaecati, distinctio repudiandae voluptatum cupiditate autem. Iure quod cum ad vel deserunt at magni, ullam, dolorum sunt atque sit, doloremque deleniti sed expedita suscipit tenetur laboriosam rerum. Eaque possimus culpa dicta asperiores eligendi corrupti, veniam repellendus.',
+  },
+  {
+    _id: 7,
+    title: 'Сколько времени нужно, чтобы освоить язык?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias, earum sit hic repellat omnis aspernatur delectus perspiciatis voluptatibus laudantium exercitationem suscipit adipisci ipsum maxime, amet, autem dolores error itaque iste! Sit impedit nisi alias delectus ullam repudiandae, dignissimos pariatur porro incidunt fuga, provident quos laborum dolore, omnis sint! Molestias fugit quos quas dolorum ratione obcaecati, distinctio repudiandae voluptatum cupiditate autem. Iure quod cum ad vel deserunt at magni, ullam, dolorum sunt atque sit, doloremque deleniti sed expedita suscipit tenetur laboriosam rerum. Eaque possimus culpa dicta asperiores eligendi corrupti, veniam repellendus.',
+  },
+  {
+    _id: 8,
+    title: 'Есть ли курсы по подготовке к экзамену DELE?',
+    answer:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias, earum sit hic repellat omnis aspernatur delectus perspiciatis voluptatibus laudantium exercitationem suscipit adipisci ipsum maxime, amet, autem dolores error itaque iste! Sit impedit nisi alias delectus ullam repudiandae, dignissimos pariatur porro incidunt fuga, provident quos laborum dolore, omnis sint! Molestias fugit quos quas dolorum ratione obcaecati, distinctio repudiandae voluptatum cupiditate autem. Iure quod cum ad vel deserunt at magni, ullam, dolorum sunt atque sit, doloremque deleniti sed expedita suscipit tenetur laboriosam rerum. Eaque possimus culpa dicta asperiores eligendi corrupti, veniam repellendus.',
+  },
+];
+
+export default FAQ_DATA;
