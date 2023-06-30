b7448001b9d7acaa4b871eb6bf1edb716bdaea89
Svetlana Logvinova <sunny7-7-7@yandex.ru>
2023-06-30 15:33:40 +0300

    fix: edit after review

--- backend/spanish_with_anna/api/urls.py
+++ backend/spanish_with_anna/api/urls.py
@@ -12,7 +12,6 @@ schema_view = get_schema_view(
         title="SpanishWithAnna API",
         default_version="v1",
         description="API of project",
-        license=openapi.License(name="BSD License"),
     ),
     public=True,
     permission_classes=(AllowAny,),
