97e211da8f204b21192b00c9a5e78bec810778a0
yryryk <tovchennikov@e1.ru>
2023-09-09 09:12:58 +0500

    fix: add button tag in MicroTooltip

--- frontend/src/components/UI/MicroTooltip/MicroTooltip.jsx
+++ frontend/src/components/UI/MicroTooltip/MicroTooltip.jsx
@@ -4,12 +4,12 @@ import MicroTooltipIcon from '../../../assets/icons/MicroTooltipIcon';
 
 function MicroTooltip({ text }) {
   return (
-    <div className={styles.container}>
+    <button type="button" className={styles.container}>
       <div className={styles.subcontainer}>
         <p className={styles.text}>{text}</p>
       </div>
       <MicroTooltipIcon />
-    </div>
+    </button>
   );
 }
 
