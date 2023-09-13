d44c605e614c23bca2fef8e969f2c11d4f49ee05
Ольга Мелихова <melihova.o@yandex.ru>
2023-09-13 19:11:09 +0300

    feat: pgadmin

--- backend/spanish_with_anna/spanish_with_anna/settings.py
+++ backend/spanish_with_anna/spanish_with_anna/settings.py
@@ -75,7 +75,7 @@ DATABASES = {
         "NAME": os.getenv("POSTGRES_DB", "postgres"),
         "USER": os.getenv("POSTGRES_USER", "postgres"),
         "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
-        "HOST": os.getenv("DB_HOST", "db"),
+        "HOST": os.getenv("DB_HOST", "postgres"),
         "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
